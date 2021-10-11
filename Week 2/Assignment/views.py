5f0a5505a32576fed15719c55fc4ba6b0bd34c11
zubaidah9-kh <92298216+zubaidah9-kh@users.noreply.github.com>
2021-10-11 11:21:50 +0300

    yes

--- Week 2/Assignment/views.py
+++ Week 2/Assignment/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def home(request):
 	# add your individual unique string into the response
-    return HttpResponse("Hello, world. 007d4a12 is the polls index.") 
+    return HttpResponse("Hello, world. 17865668  is the polls index.") 
